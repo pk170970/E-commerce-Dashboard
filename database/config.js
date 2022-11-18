f27e30b3cf207bd90bcf891749d8f8db68259f3c
Pratyush kesarwani <pk17111039@gmail.com>
2022-11-18 21:21:54 +0530

    missing mongoose package

--- database/config.js
+++ database/config.js
@@ -1,2 +1,4 @@
 const mongoose= require('mongoose');
-mongoose.connect(process.env.MONGODB_URL).then(()=> console.log("Database connection success")).catch(err=>console.log(err));
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=> console.log("Database connection success"))
+.catch(err=>console.log(err));
